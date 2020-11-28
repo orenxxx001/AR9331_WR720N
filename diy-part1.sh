90a2148ce8d387fb326a47de2bdc6a32ebc53c05
Mcxunyun666 <51842260+Mcxunyun666@users.noreply.github.com>
2020-11-28 10:14:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,27 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git ifit https://github.com/kenzok8/luci-theme-ifit.git' feeds.conf.default
+
+# Add OpenAppFilter
+cd package
+git clone https://github.com/destan19/OpenAppFilter.git
+cd -
+
+# Delete SFE
+rm package/lean/luci-app-flowoffload -fr
+rm package/lean/shortcut-fe/ -fr
+rm package/lean/luci-app-sfe/ -fr
+
+# Add Argon Theme
+cd package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+cd ..
+cd ..
