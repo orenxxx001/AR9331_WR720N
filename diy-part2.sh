6d8f4a190279ffb60db545c219da90c11286ccfb
orenxxx001 <33869335+orenxxx001@users.noreply.github.com>
2022-03-10 02:31:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,12 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 更改默认密码
+sed -i 's/root::0:0:99999:7:::/root:$1$MhPcOOTE$DOOyDUwKjP9xnoSfaczsk.:19058:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.88.8/g' package/base-files/files/bin/config_generate
+
+# 修改hostname
+sed -i 's/OpenWrt/XinV-2.0/g' package/base-files/files/bin/config_generate
+
